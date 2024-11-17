6ea97d398512ad9242945dcb07e4d56ba3a04169
Whuss88 <williamhussey95@gmail.com>
2024-11-16 20:08:47 -0500

    backend fully fucntional.

--- api/employee.js
+++ api/employee.js
@@ -1,7 +1,77 @@
 const express = require("express")
 const router = express.Router()
-module.exports = router 
+const prisma = require("../prisma")
 
-router.get("/", (req,res,next) => {
-  res.send(`Welcome to the Prismatic Employees API.`)
+router.use(express.json())
+
+router.get("/", async (req,res,next) => {
+  try {
+    const employees = await prisma.employee.findMany()
+    res.json(employees)
+    
+  } catch(e) {
+    next(e)}
+ 
 })
+
+router.get("/:id", async (req,res,next) => {
+  const {id} = req.params 
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id:parseInt(id)} })
+    if (employee) {
+      res.json(employee)
+    } else {
+      res.status(404).send(`Employee with id: ${id} not found`)
+    }
+  } catch (e){
+    next(e)
+  }
+})
+router.post("/", async (req,res,next) => {
+  const { name } = req.body
+  try {
+    const newEmployee = await prisma.employee.create({data: {name} })
+    res.status(201).json(newEmployee)
+  } catch (e){
+    next(e)
+  }
+})
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send("To update an employee, the name is required.");
+  }
+
+  try {
+    const updatedEmployee = await prisma.employee.update({
+      where: { id: parseInt(id) },
+      data: { name }
+    });
+    res.json(updatedEmployee);
+  } catch (e) {
+    if (e.code === 'P2025') { 
+      res.status(404).send(`Employee with id ${id} not found.`);
+    } else {
+      next(e);
+    }
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id: parseInt(id) } });
+    if (employee) {
+      await prisma.employee.delete({ where: { id: parseInt(id) } });
+      res.status(204).send(`Employee ${id} deleted`);
+    } else {
+      res.status(404).send(`Employee with id ${id} not found`);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,12 +9,25 @@ app.use((req,res,next) => {
 })
 
 
-
-app.use("/employees", require("./api/employee"))
-
 app.get("/", (req,res,next) => {
   res.send(`Welcome to the Prismatic Employees API.`)
 })
+
+app.use("/employees", require("./api/employee"))
+
+app.use((req,res,next) => {
+  next({
+    status: 404,
+    message: `The endpoint ${req.method} ${req.originalUrl} doesn't exist.`
+  })
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.status ?? 500)
+  res.send(err.message ?? "Sorry! Something went wrong :[")
+})
+
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`)
